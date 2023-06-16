94c341b1e7c1487a35537863f81ecef37bf2e7da
Artem <artem.matseruk@gmail.com>
2023-06-16 17:22:19 +0300

    feat: added favicons, open graph

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import { PageLayout } from '@/app/layouts/PageLayout'
-import { HomeContent } from '@/modules/Home'
+// import { HomeContent } from '@/modules/Home'
 
 export default function Home() {
   return (
-    <PageLayout headerTheme="dark">
+    <PageLayout headerTheme="light">
       {/*<HomeContent />*/}
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <h1>Coming soon</h1>
+      </div>
       <div className="root"></div>
     </PageLayout>
   )
--- src/modules/BecomeADistributor/ui/BecomeADistributorContent.tsx
+++ src/modules/BecomeADistributor/ui/BecomeADistributorContent.tsx
@@ -1,10 +1,19 @@
+// import { useRef } from 'react'
 import Container from '@/app/layouts/Container'
 import BecomeADistributorForm from '@/modules/BecomeADistributor/ui/BecomeADistributorForm/BecomeADistributorForm'
 import HeroInner from '@/ui/HeroInner/HeroInner'
+// import { motion, useTransform, useScroll } from 'framer-motion'
 
 import styles from './BecomeADistributorContent.module.scss'
 
 export const BecomeADistributorContent = () => {
+  // const targetRef = useRef<HTMLDivElement | null>(null)
+  // const { scrollYProgress } = useScroll({
+  //   target: targetRef,
+  //   offset: ['start end', 'end start'],
+  // })
+
+  // const y2 = useTransform(scrollYProgress, [0, 0.5, 1], [1.2, 1, 1.5])
   return (
     <main className={styles['becomeADistributorContent']}>
       <HeroInner
@@ -18,7 +27,39 @@ export const BecomeADistributorContent = () => {
           <div className="col-md-6 order-md-2">
             <BecomeADistributorForm />
           </div>
-          <div className="col-md-5 order-md-1">Description + image</div>
+          <div className="col-md-5 order-md-1">
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<div>*/}
+            {/*  <div*/}
+            {/*    style={{*/}
+            {/*      width: '250rem',*/}
+            {/*      height: '400rem',*/}
+            {/*      overflow: 'hidden',*/}
+            {/*    }}*/}
+            {/*    data-title="target"*/}
+            {/*    ref={targetRef}*/}
+            {/*  >*/}
+            {/*    <motion.img*/}
+            {/*      src="https://images.pexels.com/photos/16791877/pexels-photo-16791877/free-photo-of-wood-art-texture-garden.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"*/}
+            {/*      alt="Alt"*/}
+            {/*      style={{ scale: y2 }}*/}
+            {/*    />*/}
+            {/*  </div>*/}
+            {/*</div>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+            {/*<h1>space</h1>*/}
+          </div>
         </div>
       </Container>
     </main>
