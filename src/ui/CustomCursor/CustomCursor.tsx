b969457bf2b6bfc7ae7176a35616c07c813c3fef
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-06-21 10:22:37 +0300

    feat: add custom cursor

--- src/ui/CustomCursor/CustomCursor.tsx
+++ src/ui/CustomCursor/CustomCursor.tsx
@@ -2,25 +2,21 @@ import React, { useEffect, useState } from 'react'
 import styles from './CustomCursor.module.scss'
 import { motion } from 'framer-motion'
 import { useScrollPosition } from '@/hooks/useScrollPosition'
+import { useRouter } from 'next/router'
 
 const CustomCursor: React.FC = () => {
   const [cursorVariant, setCursorVariant] = useState('default')
   const { scrollPosition: mousePosition } = useScrollPosition({ x: 0, y: 0 })
+  const { route } = useRouter()
 
   const variants = {
     default: {
-      width: 16,
-      height: 16,
+      width: 11,
+      height: 11,
       x: mousePosition.x,
       y: mousePosition.y,
     },
-    onLink: {
-      width: 24,
-      height: 24,
-      x: mousePosition.x,
-      y: mousePosition.y,
-    },
-    onInput: {
+    onSubject: {
       width: 0,
       height: 0,
       opacity: 0,
@@ -33,36 +29,26 @@ const CustomCursor: React.FC = () => {
     type: 'spring',
     mass: 0.6,
     stiffness: 500,
-    damping: 35,
+    damping: 28,
   }
 
-  const onLink = () => {
-    setCursorVariant('onLink')
+  const onSubject = () => {
+    setCursorVariant('onSubject')
   }
 
   const leaveDefault = () => {
     setCursorVariant('default')
   }
 
-  const onInput = () => {
-    setCursorVariant('onInput')
-    console.log('hi')
-  }
-
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const links = document.querySelectorAll('a, button')
+      const links = document.querySelectorAll('a, button, input, textarea')
       links.forEach((link) => {
-        link.addEventListener('mouseenter', onLink)
+        link.addEventListener('mouseenter', onSubject)
         link.addEventListener('mouseleave', leaveDefault)
       })
-      const inputs = document.querySelectorAll('input, textarea')
-      inputs.forEach((input) => {
-        input.addEventListener('mouseenter', onInput)
-        input.addEventListener('mouseleave', leaveDefault)
-      })
     }
-  }, [])
+  }, [route])
 
   return (
     <motion.div
