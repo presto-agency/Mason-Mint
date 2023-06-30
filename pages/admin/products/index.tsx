c0d9a962c920f20577b43c0dc726aebb82cbe214
Artem <artem.matseruk@gmail.com>
2023-06-30 18:06:26 +0300

    update: move GET products to client side

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -1,23 +1,34 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import { PageLayout } from '@/app/layouts/PageLayout'
 import { AdminProducts } from '@/modules/Admin'
-import db from '@/utils/db'
-import ProductModel from '../../../models/Product'
+// import db from '@/utils/db'
+// import ProductModel from '../../../models/Product'
 
-export default function ProductsPage({ products }: { products: string }) {
+// export default function ProductsPage({ products }: { products: string }) {
+export default function ProductsPage() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`${window.location.origin}/api/products`)
+      .then((res) => setProducts(res.data.data))
+  }, [])
+
   return (
     <PageLayout>
-      <AdminProducts products={JSON.parse(products)} />
+      <AdminProducts products={products} />
     </PageLayout>
   )
 }
 
-export const getServerSideProps = async () => {
-  await db.connect()
-  const products = await ProductModel.find()
-  await db.disconnect()
-  return {
-    props: {
-      products: JSON.stringify(products.map(db.convertDocToObj)),
-    },
-  }
-}
+// export const getServerSideProps = async () => {
+//   await db.connect()
+//   const products = await ProductModel.find()
+//   await db.disconnect()
+//   return {
+//     props: {
+//       products: JSON.stringify(products.map(db.convertDocToObj)),
+//     },
+//   }
+// }
