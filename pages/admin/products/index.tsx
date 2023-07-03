560effb4016035d0377211d96bd9cdc280b59019
Artem <artem.matseruk@gmail.com>
2023-07-03 10:24:58 +0300

    update: set MongoDB on vercel

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -2,36 +2,32 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { PageLayout } from '@/app/layouts/PageLayout'
 import { AdminProducts } from '@/modules/Admin'
-// import db from '@/utils/db'
-// import ProductModel from '../../../models/Product'
+import db from '@/utils/db'
+import ProductModel from '../../../models/Product'
 
-// export default function ProductsPage({ products }: { products: string }) {
-export default function ProductsPage() {
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    // axios
-    //   .get(`${window.location.origin}/api/products`)
-    //   .then((res) => setProducts(res.data.data))
-    axios
-      .get('https://jsonplaceholder.typicode.com/comments')
-      .then((res) => console.log('res ', res))
-  }, [])
+export default function ProductsPage({ products }: { products: string }) {
+  // const [products, setProducts] = useState([])
+  //
+  // useEffect(() => {
+  //   axios
+  //     .get(`${window.location.origin}/api/products`)
+  //     .then((res) => setProducts(res.data.data))
+  // }, [])
 
   return (
     <PageLayout>
-      <AdminProducts products={[]} />
+      <AdminProducts products={JSON.parse(products)} />
     </PageLayout>
   )
 }
 
-// export const getServerSideProps = async () => {
-//   await db.connect()
-//   const products = await ProductModel.find()
-//   await db.disconnect()
-//   return {
-//     props: {
-//       products: JSON.stringify(products.map(db.convertDocToObj)),
-//     },
-//   }
-// }
+export const getServerSideProps = async () => {
+  await db.connect()
+  const products = await ProductModel.find()
+  await db.disconnect()
+  return {
+    props: {
+      products: JSON.stringify(products.map(db.convertDocToObj)),
+    },
+  }
+}
