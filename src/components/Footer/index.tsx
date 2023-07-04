b34bb08e323640aa4f4789bd7b3d61e6c4dbf458
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-07-04 21:12:20 +0300

    fix: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -16,58 +16,55 @@ const LogoBlock = () => {
   )
 }
 
+const navigationFooterLinks = [
+  {
+    category: 'sitemap',
+    links: [
+      { title: 'About Us', href: '' },
+      { title: 'Contact Us', href: '' },
+    ],
+  },
+  {
+    category: 'products',
+    links: [
+      { title: 'Designs', href: '' },
+      { title: 'Custom Minting', href: '' },
+      { title: 'Packaging', href: '' },
+    ],
+  },
+  {
+    category: 'follow us',
+    links: [
+      { title: 'Facebook', href: '' },
+      { title: 'Instagram', href: '' },
+      { title: 'Twitter', href: '' },
+      { title: 'Linkedin', href: '' },
+    ],
+  },
+]
+
 const NavigationBlock = () => {
   return (
-    <nav className={styles.navigation}>
-      <div className={styles.navigation__left}>
-        <div className={styles.navigation__left_list}>
-          <h6 className={styles.title}>sitemap</h6>
-          <div className={styles.title__items}>
-            <Link className={styles.title__items_link} href={'/about'}>
-              ABOUT US
-            </Link>
-            <Link className={styles.title__items_link} href={'/'}>
-              CONTACT US
-            </Link>
-          </div>
+    <div className={styles['navigation']}>
+      {navigationFooterLinks.map((item) => (
+        <div className={styles['navigation__column']} key={item.category}>
+          <h6 className={classNames('h6', styles['navigation__column_title'])}>
+            {item.category}
+          </h6>
+          <nav className={styles['links']}>
+            {item.links.map((link) => (
+              <Link
+                className={styles['links__link']}
+                href={link.href}
+                key={link.title}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </nav>
         </div>
-        <div className={styles.navigation__left_list}>
-          <h6 className={styles.title}>products</h6>
-          <div className={styles.title__items}>
-            <Link className={styles.title__items_link} href={'/'}>
-              DESIGNS
-            </Link>
-            <Link className={styles.title__items_link} href={'/'}>
-              CUSTOM MINTING
-            </Link>
-            <Link className={styles.title__items_link} href={'/'}>
-              PACKAGING
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className={styles.navigation__left_list}>
-        <h6 className={styles.title}>follow us</h6>
-        <div className={classNames(styles.title__items)}>
-          <div className={styles.title__items}>
-            <Link className={styles.title__items_link} href={'/'}>
-              FACEBOOK
-            </Link>
-            <Link className={styles.title__items_link} href={'/'}>
-              INSTAGRAM
-            </Link>
-          </div>
-          <div className={styles.title__items}>
-            <Link className={styles.title__items_link} href={'/'}>
-              TWITTER
-            </Link>
-            <Link className={styles.title__items_link} href={'/'}>
-              LINKEDIN
-            </Link>
-          </div>
-        </div>
-      </div>
-    </nav>
+      ))}
+    </div>
   )
 }
 
