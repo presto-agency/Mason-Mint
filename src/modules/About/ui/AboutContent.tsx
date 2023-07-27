b0068c09256e5d3bee86ab862afe313ea69a0a09
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-07-27 17:44:17 +0300

    feat: create heroDetail

--- src/modules/About/ui/AboutContent.tsx
+++ src/modules/About/ui/AboutContent.tsx
@@ -3,12 +3,20 @@ import HeroInner from '@/ui/HeroInner/HeroInner'
 import WasBorn from '@/modules/About/ui/WasBorn/WasBorn'
 import AllProducts from '@/modules/About/ui/AllProducts/AllProducts'
 import BecomeDistributorSection from '@/components/BecomeDistributorSection/BecomeDistributorSection'
-import WhatWeDo from '@/modules/About/ui/WhatWeDo/WhatWeDo'
+import WhatWeDo from '@/components/WhatWeDo/WhatWeDo'
 import dynamic from 'next/dynamic'
-const FindPartner = dynamic(
-  () => import('@/modules/About/ui/FindPartner/FindPartner'),
-  { ssr: false }
-)
+const HeroDetail = dynamic(() => import('@/ui/HeroDetail/HeroDetail'), {
+  ssr: false,
+})
+
+const sliderImages = [
+  '/images/about/innerAbout.jpg',
+  '/images/home/customDesign/slide_1.jpg',
+  '/images/home/customDesign/slide_2.jpg',
+  '/images/home/customDesign/slide_3.jpg',
+  '/images/home/customDesign/slide_4.jpg',
+  '/images/home/customDesign/slide_5.jpg',
+]
 
 import styles from './AboutContent.module.scss'
 
@@ -20,7 +28,15 @@ export const AboutContent: FC = () => {
         subtitle="Welcome to masonmint"
         columns={10}
       />
-      <FindPartner />
+      <HeroDetail
+        sliderImages={sliderImages}
+        image="/images/about/innerAbout.jpg"
+        topDescription="We’ve built our business on long-term relationship with the
+                  world’s largest mining companies"
+        bottomDescription="Providing our partners with reliable and efficient results
+                  supported by the resources of a publicly listed, global
+                  company."
+      />
       <WasBorn />
       <AllProducts />
       <WhatWeDo />
--- src/modules/Packaging/ui/PackagingContent.tsx
+++ src/modules/Packaging/ui/PackagingContent.tsx
@@ -1,12 +1,37 @@
-import { FC } from 'react'
+import React, { FC } from 'react'
 import HeroInner from '@/ui/HeroInner/HeroInner'
-
+import WhatWeDo from '@/components/WhatWeDo/WhatWeDo'
+import BecomeDistributorSection from '@/components/BecomeDistributorSection/BecomeDistributorSection'
+const HeroDetail = dynamic(() => import('@/ui/HeroDetail/HeroDetail'), {
+  ssr: false,
+})
 import styles from './PackagingContent.module.scss'
+import dynamic from 'next/dynamic'
+
+const sliderImages = [
+  '/images/packaging/hero.jpg',
+  '/images/home/customDesign/slide_1.jpg',
+  '/images/home/customDesign/slide_2.jpg',
+  '/images/home/customDesign/slide_3.jpg',
+  '/images/home/customDesign/slide_4.jpg',
+  '/images/home/customDesign/slide_5.jpg',
+]
 
 export const PackagingContent: FC = () => {
   return (
     <main className={styles['PackagingContent']}>
-      <HeroInner title="Coming soon" subtitle="Packaging" />
+      <HeroInner
+        title="Creation of any packaging is always the coin"
+        subtitle="Packaging"
+        description="It inspires the creative concepts for the narrative, and then the selection of the appropriate form and design. "
+        columns={9}
+      />
+      <HeroDetail
+        sliderImages={sliderImages}
+        image="/images/packaging/hero.jpg"
+      />
+      <WhatWeDo />
+      <BecomeDistributorSection />
     </main>
   )
 }
