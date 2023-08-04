4acf11272edd1cc5b36437bb161ec37ae1ab8c64
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-08-04 12:08:53 +0300

    feat: add some page transition#2

--- src/app/layouts/PageTransitionLayout/index.tsx
+++ src/app/layouts/PageTransitionLayout/index.tsx
@@ -5,21 +5,21 @@ const variants = {
   initial: {
     opacity: 0,
     transition: {
-      duration: 1.5,
+      duration: 0.5,
       ease: 'easeInOut',
     },
   },
   animate: {
     opacity: 1,
     transition: {
-      duration: 1.5,
+      duration: 0.5,
       ease: 'easeInOut',
     },
   },
   exit: {
     opacity: 0,
     transition: {
-      duration: 1.5,
+      duration: 0.5,
       ease: 'easeInOut',
     },
   },
