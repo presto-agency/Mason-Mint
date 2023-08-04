9b706a7b6b4c1555219e10bb4224547ffec1f6e8
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-08-04 18:34:53 +0300

    fix: page transition#3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,28 @@ import AppLayout from '@/app/layouts/AppLayout'
 
 import 'bootstrap/scss/bootstrap-grid.scss'
 import '@/app/styles/index.scss'
+import { useNextCssRemovalPrevention } from '@madeinhaus/nextjs-page-transition'
+import { AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const onExitComplete = () => {
+    window.scrollTo({ top: 0 })
+  }
+  useNextCssRemovalPrevention()
+  const router = useRouter()
+  const pageKey = router.asPath
   return (
     <StoreProvider>
       <AppLayout>
         <NextNProgress color="#266ef9" />
-        <Component {...pageProps} />
+        <AnimatePresence
+          onExitComplete={onExitComplete}
+          mode="wait"
+          initial={false}
+        >
+          <Component {...pageProps} key={pageKey} />
+        </AnimatePresence>
       </AppLayout>
     </StoreProvider>
   )
--- pages/designs/[productId]/[slug].tsx
+++ pages/designs/[productId]/[slug].tsx
@@ -3,11 +3,9 @@ import PageTransitionLayout from '@/app/layouts/PageTransitionLayout'
 import { useRouter } from 'next/router'
 
 const Index = () => {
-  const router = useRouter()
-  const pageKey = router.asPath
   return (
     <PageTransitionLayout>
-      <DesignsDetailContent key={pageKey} />
+      <DesignsDetailContent />
     </PageTransitionLayout>
   )
 }
