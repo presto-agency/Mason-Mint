54dc1f4a5753df3378ba7367b286a8a5f8ed4ede
Artem <artem.matseruk@gmail.com>
2023-08-07 12:23:09 +0300

    feat: added main preloader

--- src/components/MainPreloader/MainPreloader.tsx
+++ src/components/MainPreloader/MainPreloader.tsx
@@ -4,13 +4,27 @@ import Lottie from 'lottie-react'
 import loaderJson from './assets/Loader.json'
 
 import styles from './MainPreloader.module.scss'
+import classNames from 'classnames'
 
 const MainPreloader = () => {
-  // const [isLoaded, setIsLoaded] = useState<boolean>(true)
+  const [isLoaded, setIsLoaded] = useState<boolean>(true)
+  const [progress, setProgress] = useState<number>(0)
 
-  const single = {}
-  const tenths = {}
-  const hunfredths = {}
+  useEffect(() => {
+    const progressInterval = setInterval(() => {
+      setProgress((prevState) => {
+        if (prevState === 3) {
+          clearInterval(progressInterval)
+          console.log('end')
+          return prevState
+        }
+        return prevState + 1
+      })
+    }, 800)
+    return () => {
+      clearInterval(progressInterval)
+    }
+  }, [])
 
   return (
     <motion.div
@@ -22,22 +36,31 @@ const MainPreloader = () => {
         <span>Please wait</span>
         <span>we make money</span>
       </div>
-      {/*<motion.div*/}
-      {/*  className={styles['preloader__thumb']}*/}
-      {/*  initial={{ opacity: 0 }}*/}
-      {/*  animate={isLoaded ? { opacity: 1 } : {}}*/}
-      {/*  exit={{ opacity: 0 }}*/}
-      {/*  transition={{ duration: 0.5 }}*/}
-      {/*>*/}
-      {/*  <Lottie*/}
-      {/*    className={styles['preloader__thumb_item']}*/}
-      {/*    animationData={loaderJson}*/}
-      {/*    autoplay={true}*/}
-      {/*    onLoadedImages={() => setIsLoaded(true)}*/}
-      {/*  />*/}
-      {/*</motion.div>*/}
-      <p className={styles['preloader__title']}>Page is rolling...</p>
-      <div className={styles['preloader__percent']}>
+      <motion.div
+        className={styles['preloader__thumb']}
+        initial={{ opacity: 0 }}
+        animate={isLoaded ? { opacity: 1 } : {}}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Lottie
+          className={styles['preloader__thumb_item']}
+          animationData={loaderJson}
+          autoplay={true}
+          onLoadedImages={() => setIsLoaded(true)}
+        />
+      </motion.div>
+      <p className={styles['preloader__title']}>
+        Page is rolling<span className={styles['preloader__title_dot']}>.</span>
+        <span className={styles['preloader__title_dot']}>.</span>
+        <span className={styles['preloader__title_dot']}>.</span>
+      </p>
+      <div
+        className={classNames(
+          styles['preloader__percent'],
+          styles[`__${progress}`]
+        )}
+      >
         <div className={styles['preloader__percent_item']}>
           <div className={styles['preloader__number']}>1</div>
         </div>
@@ -47,6 +70,7 @@ const MainPreloader = () => {
           <div className={styles['preloader__number']}>3</div>
         </div>
         <div className={styles['preloader__percent_item']}>
+          <div className={styles['preloader__number']}>0</div>
           <div className={styles['preloader__number']}>0</div>
           <div className={styles['preloader__number']}>0</div>
           <div className={styles['preloader__number']}>0</div>
