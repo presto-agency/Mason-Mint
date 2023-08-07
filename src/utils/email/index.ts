f685be7307728ea25619f1e9dd1095579f4f3f0b
Artem <artem.matseruk@gmail.com>
2023-08-07 16:57:25 +0300

    update: remove connectionTimeout from nodemailer settings

--- src/utils/email/index.ts
+++ src/utils/email/index.ts
@@ -14,7 +14,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  connectionTimeout: 5000,
+  // connectionTimeout: 5000,
 } as TransportOptions)
 
 export const sendEmail = async ({
