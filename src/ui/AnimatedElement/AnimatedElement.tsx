f91ee4ebb0247f5cfd86008e70de25399e50d91d
Artem <artem.matseruk@gmail.com>
2023-08-09 16:58:46 +0300

    update: change transition type for AnimatedElement

--- src/ui/AnimatedElement/AnimatedElement.tsx
+++ src/ui/AnimatedElement/AnimatedElement.tsx
@@ -26,8 +26,7 @@ const AnimatedElement: FC<AnimatedElementProps> = ({
       opacity: 1,
       y: 0,
       transition: {
-        ease: 'anticipate',
-        duration: 1,
+        duration: 0.5,
         delay,
       },
     },
