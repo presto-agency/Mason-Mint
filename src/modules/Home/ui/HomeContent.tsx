c02d78855128d2eafb7b099477c3018b6e9f4c2f
Artem <artem.matseruk@gmail.com>
2023-08-11 14:40:04 +0300

    update: hide some sections on home

--- src/modules/Home/ui/HomeContent.tsx
+++ src/modules/Home/ui/HomeContent.tsx
@@ -4,16 +4,16 @@ import StorySection from './StorySection/StorySection'
 
 import FAQSection from '@/modules/Home/ui/FAQSection/FAQSection'
 import BecomeDistributorSection from '@/components/BecomeDistributorSection/BecomeDistributorSection'
-// import SellSection from '@/modules/Home/ui/SellSection/SellSection'
-// const FeaturedDesignsSection = dynamic(
-//   () =>
-//     import('@/modules/Home/ui/FeaturedDesignsSection/FeaturedDesignsSection'),
-//   { ssr: false }
-// )
-// const ExploreDesignsSection = dynamic(
-//   () => import('@/modules/Home/ui/ExploreDesignsSection/ExploreDesignsSection'),
-//   { ssr: false }
-// )
+import SellSection from '@/modules/Home/ui/SellSection/SellSection'
+const FeaturedDesignsSection = dynamic(
+  () =>
+    import('@/modules/Home/ui/FeaturedDesignsSection/FeaturedDesignsSection'),
+  { ssr: false }
+)
+const ExploreDesignsSection = dynamic(
+  () => import('@/modules/Home/ui/ExploreDesignsSection/ExploreDesignsSection'),
+  { ssr: false }
+)
 const CustomDesignsSection = dynamic(
   () => import('@/modules/Home/ui/CustomDesignsSection/CustomDesignsSection'),
   { ssr: false }
@@ -26,10 +26,10 @@ const HomeContent = () => {
     <main className={styles['HomeContent']}>
       <IntroSection />
       <StorySection />
-      {/*<ExploreDesignsSection />*/}
-      {/*<FeaturedDesignsSection />*/}
+      <ExploreDesignsSection />
+      <FeaturedDesignsSection />
       <CustomDesignsSection />
-      {/*<SellSection />*/}
+      <SellSection />
       <FAQSection />
       <BecomeDistributorSection />
     </main>
